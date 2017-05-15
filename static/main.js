baae9aadbc657aa0eb28d1eae51eccdbfce9948b
jswh <jswh@jswh.me>
2017-05-15 15:26:27 +0800

    new data save

--- static/main.js
+++ static/main.js
@@ -22,22 +22,11 @@ ready(function () {
 
         var editor = document.getElementById('editor-area');
         var title = document.getElementById('title-area')
-        initEditor(title, editor);
         var previewer = document.getElementById('previewer');
+
+        initEditor(title, editor, previewer);
+
         previewer.innerHTML = markdown.toHTML(editor.value);
-        editor.focus();
-        editor.addEventListener('blur', function () {
-            //editor.focus();
-        }, false);
-        var lastRender = 0;
-        editor.addEventListener('input', function () {
-            var now = Date.now();
-            if ((now - lastRender) > 100) {
-                lastRender = now;
-                localStorage.setItem('data', editor.value);
-                previewer.innerHTML = markdown.toHTML(editor.value);
-            }
-        }, false);
         document.getElementsByClassName('small')[0].addEventListener('dblclick', switchView)
     }
     function switchView() {
@@ -46,7 +35,7 @@ ready(function () {
         small.className = 'large';
         large.className = 'small';
     }
-    function initEditor(title, editor) {
+    function initEditor(title, editor, previewer) {
         var storeKey = 'article';
         var article = JSON.parse(localStorage.getItem(storeKey));
         if (!article) {
@@ -57,59 +46,22 @@ ready(function () {
         }
         title.value = article.title;
         editor.value = article.content;
-        editor.addEventListener('keydown', function (e) {
-            return;
-            if (!e.vimEmulate) {
-                if (vimEmulate(e.key, e.target)) {
-                    e.preventDefault();
-                }
-            }
-        })
-    }
+        window.vim = Vim(editor);
 
-    function vimEmulate(key, target) {
-        var keyMapping = {
-            h: 'ArrowLeft',
-            j: 'ArrowDown',
-            k: 'ArrowUp',
-            l: 'ArrowRight'
-        }
-        var action = keyMapping[key]
-        if (action) {
-            if (typeof action == 'string') {
-                keypress(key, 1, target);
+        var lastRender = 0;
+        editor.addEventListener('input', saveAndRender, false);
+        function saveAndRender(e) {
+            article.title = title.value;
+            article.content = editor.value;
+            var now = Date.now();
+            if ((now - lastRender) > 100) {
+                lastRender = now;
+                previewer.innerHTML = markdown.toHTML(article.content);
+                localStorage.setItem(storeKey, JSON.stringify(article));
             }
-            return true
         }
-        return false;
-
     }
 });
-function keypress(key, repeat, target) {
-    console.log(this);
-    var evt = document.createEvent("KeyboardEvent");
-    var initFunc = evt.initKeyEvent || evt.initKeyboardEvent
-    initFunc(
-        "keypress", // event type : keydown, keyup, keypress
-        true, // bubbles
-        true, // cancelable
-        target, // viewArg: should be window
-        false, // ctrlKeyArg
-        false, // altKeyArg
-        false, // shiftKeyArg
-        false, // metaKeyArg
-        40, // keyCodeArg : unsigned long the virtual key code, else 0
-        0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
-    )
-    var canceled = !target.dispatchEvent(evt);
-    if (canceled) {
-        // A handler called preventDefault
-        alert("canceled");
-    } else {
-        // None of the handlers called preventDefault
-        alert("not canceled");
-    }
-}
 function ready(fn) {
     document.addEventListener('DOMContentLoaded', fn, false);
 }
